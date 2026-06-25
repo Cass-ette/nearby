9566448b0a39bab9a6716a26bea88c28db89854c
Cass-ette <54059865+Cass-ette@users.noreply.github.com>
2026-06-26 00:01:49 +0800

    feat: add color palette

---
+++ 附近/DesignSystem/Color+Nearby.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+extension Color {
+    // Paper backgrounds
+    static let paper50 = Color(red: 0xFA/255, green: 0xF6/255, blue: 0xEE/255)
+    static let paper100 = Color(red: 0xF2/255, green: 0xEC/255, blue: 0xDD/255)
+    static let paper200 = Color(red: 0xE8/255, green: 0xDF/255, blue: 0xC9/255)
+
+    // Ink text
+    static let ink900 = Color(red: 0x2A/255, green: 0x25/255, blue: 0x20/255)
+    static let ink700 = Color(red: 0x4A/255, green: 0x41/255, blue: 0x3A/255)
+    static let ink500 = Color(red: 0x84/255, green: 0x7A/255, blue: 0x6F/255)
+    static let ink300 = Color(red: 0xB5/255, green: 0xA9/255, blue: 0x9B/255)
+
+    // Accent (cinnabar)
+    static let cinnabar = Color(red: 0xB5/255, green: 0x56/255, blue: 0x3F/255)
+
+    // Mood colors
+    static let moodSerene = Color(red: 0xB5/255, green: 0xC4/255, blue: 0xB1/255)      // 宁静
+    static let moodCurious = Color(red: 0xC4/255, green: 0xB5/255, blue: 0x8A/255)     // 好奇
+    static let moodMelancholy = Color(red: 0x8C/255, green: 0x9C/255, blue: 0xB5/255)  // 惆怅
+    static let moodTender = Color(red: 0xD4/255, green: 0xB5/255, blue: 0xB5/255)      // 温柔
+    static let moodSurprise = Color(red: 0xD4/255, green: 0xB5/255, blue: 0x8A/255)    // 惊喜
+}
